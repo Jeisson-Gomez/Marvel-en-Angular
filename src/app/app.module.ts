27280aa5b1dbaef0b5a7fd00ade4b69a3d95e2a5
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2022-10-18 14:19:19 -0500

    Actualiza el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 
+//componentes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
